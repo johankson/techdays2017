6ac15ee56c0a893191280f43f63ef4f35962b3c8
Johan Karlsson <johan.kson@gmail.com>
2017-10-15 11:59:32 +0200

    Clickable animation

--- src/demo/LottiePage.xaml.cs
+++ src/demo/LottiePage.xaml.cs
@@ -10,7 +10,24 @@ namespace demo
         public LottiePage()
         {
             InitializeComponent();
-            TheButton.Clicked += (sender, e) => AnimationView.Play();
+            ClickBox.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    if(AnimationView.IsPlaying)
+                    {
+                        return;
+                    }
+
+                    if(AnimationView.Progress < 0.1)
+                    {
+						this.Animate("forward", (d) => AnimationView.Progress = (float)d, 0, 0.2, 60, 500); 
+                    }
+                    else
+                    {
+                        this.Animate("backward", (d) => AnimationView.Progress = (float)d, 0.2, 0, 60, 500); 
+                    }
+                })
+            });
         }
     }
 }
