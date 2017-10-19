d8e1d1a8f3b8acaa61781d77e6ad8ab19d49e274
Johan Karlsson <johan.kson@gmail.com>
2017-10-19 03:33:17 +0200

    Minor touchups

--- src/demo/LottiePage.xaml.cs
+++ src/demo/LottiePage.xaml.cs
@@ -10,24 +10,24 @@ namespace demo
         public LottiePage()
         {
             InitializeComponent();
-            ClickBox.GestureRecognizers.Add(new TapGestureRecognizer()
-            {
-                Command = new Command(() => {
-                    if (AnimationView.IsPlaying)
-                    {
-                        return;
-                    }
+      //      ClickBox.GestureRecognizers.Add(new TapGestureRecognizer()
+      //      {
+      //          Command = new Command(() => {
+      //              if (AnimationView.IsPlaying)
+      //              {
+      //                  return;
+      //              }
 
-                    if (AnimationView.Progress < 0.1)
-                    {
-						this.Animate("forward", (d) => AnimationView.Progress = (float)d, 0, 0.2, 60, 500); 
-                    }
-                    else
-                    {
-                        this.Animate("backward", (d) => AnimationView.Progress = (float)d, 0.2, 0, 60, 500); 
-                    }
-                })
-            });
+      //              if (AnimationView.Progress < 0.1)
+      //              {
+						//this.Animate("forward", (d) => AnimationView.Progress = (float)d, 0, 0.2, 60, 500); 
+            //        }
+            //        else
+            //        {
+            //            this.Animate("backward", (d) => AnimationView.Progress = (float)d, 0.2, 0, 60, 500); 
+            //        }
+            //    })
+            //});
         }
     }
 }
